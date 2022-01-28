aa4095361e5bc345f263f7cbff15ac19997d92fc
Hugo Faria Lima <hugofaria157@live.com>
2022-01-27 21:47:15 -0300

    colocando pagina 404 personalizada

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import React from 'react'
+import { useRouter } from 'next/router'
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  )
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1'
@@ -62,17 +33,23 @@ function Titulo(props) {
 //export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  //const username = 'peas'
+  const [username, setUsername] = React.useState('dreackdown')
+  const roteamento = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+          backgroundImage:
+            'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundBlendMode: 'multiply'
         }}
       >
         <Box
@@ -82,52 +59,91 @@ export default function PaginaInicial() {
             justifyContent: 'space-between',
             flexDirection: {
               xs: 'column',
-              sm: 'row',
+              sm: 'row'
             },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
+            width: '100%',
+            maxWidth: '700px',
+            borderRadius: '5px',
+            padding: '32px',
+            margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
+            backgroundColor: appConfig.theme.colors.neutrals[700]
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault()
+              console.log('Alguém submeteu o form')
+              roteamento.push('/chat')
+              //window.location.href = '/chat'
+            }}
             styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: { xs: '100%', sm: '50%' },
+              textAlign: 'center',
+              marginBottom: '32px'
             }}
           >
             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Text
+              variant="body3"
+              styleSheet={{
+                marginBottom: '32px',
+                color: appConfig.theme.colors.neutrals[300]
+              }}
+            >
               {appConfig.name}
             </Text>
 
+            {/*<input
+              type="text"
+              value={username}
+              onChange={function (event) {
+                console.log('usuário digitou', event.target.value)
+                //onda ta o valor?
+                const valor = event.target.value
+                //Trocar o valor da variavel
+                setUsername(valor)
+              }}
+            /> */}
+
             <TextField
+              value={username}
+              onChange={function (event) {
+                console.log('usuário digitou', event.target.value)
+                //onda ta o valor?
+                const valor = event.target.value
+                //Trocar o valor da variavel
+                setUsername(valor)
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
                   mainColor: appConfig.theme.colors.neutrals[900],
                   mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
-                },
+                  backgroundColor: appConfig.theme.colors.neutrals[800]
+                }
               }}
             />
             <Button
-              type='submit'
-              label='Entrar'
+              type="submit"
+              label="Entrar"
               fullWidth
               buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
+                contrastColor: appConfig.theme.colors.neutrals['000'],
                 mainColor: appConfig.theme.colors.primary[500],
                 mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
+                mainColorStrong: appConfig.theme.colors.primary[600]
               }}
             />
           </Box>
           {/* Formulário */}
-
 
           {/* Photo Area */}
           <Box
@@ -142,13 +158,13 @@ export default function PaginaInicial() {
               borderColor: appConfig.theme.colors.neutrals[999],
               borderRadius: '10px',
               flex: 1,
-              minHeight: '240px',
+              minHeight: '240px'
             }}
           >
             <Image
               styleSheet={{
                 borderRadius: '50%',
-                marginBottom: '16px',
+                marginBottom: '16px'
               }}
               src={`https://github.com/${username}.png`}
             />
@@ -168,5 +184,5 @@ export default function PaginaInicial() {
         </Box>
       </Box>
     </>
-  );
+  )
 }
